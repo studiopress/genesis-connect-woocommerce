65257e0e17d7a012f23ffe9f4e7afee3b9caadbe
Nick Cernis <nick@cern.is>
2018-08-01 20:32:00 +0200

    Fix Woo tested up to version

--- genesis-connect-woocommerce.php
+++ genesis-connect-woocommerce.php
@@ -7,7 +7,7 @@ Author: StudioPress
 Author URI: https://www.studiopress.com/
 Description: Allows you to seamlessly integrate WooCommerce with the Genesis Framework and Genesis child themes.
 WC requires at least: 3.3.0
-WC tested up to: 4.9
+WC tested up to: 3.4
 
 License: GNU General Public License v2.0 (or later)
 License URI: http://www.opensource.org/licenses/gpl-license.php
