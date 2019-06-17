abb3ba6a8eb139090baef7f60a8157fe45dab692
Ryan Meier <rfmeier@gmail.com>
2019-06-17 13:53:44 -0500

    Updated admin/notices.php for WordPress code standards.

--- admin/notices.php
+++ admin/notices.php
@@ -2,15 +2,32 @@
 /**
  * Callbacks for `admin_notices` action to load HTML notices.
  *
- * @package genesis_connect_woocommerce
- * @version 1.0
+ * @package Genesis_Connect_WooCommerce
+ * @since 1.0
  */
-defined( 'ABSPATH' ) || exit;
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Display notice message if WooCommerce is not active.
+ *
+ * Callback for WordPress 'admin_notices' action.
+ *
+ * @since 1.0
+ */
 function gencwooc_woocommerce_notice() {
 	include GCW_ADMIN_DIR . '/views/html-notice-needs-woocommerce.php';
 }
 
+/**
+ * Display notice message if Genesis is not active.
+ *
+ * Callback for WordPress 'admin_notices' action.
+ *
+ * @since 1.0
+ */
 function gencwooc_genesis_notice() {
 	include GCW_ADMIN_DIR . '/views/html-notice-needs-genesis.php';
 }
