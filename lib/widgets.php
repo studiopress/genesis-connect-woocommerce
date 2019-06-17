6868fdbc37d432dc16e8632548831dc2f5452fb8
Ryan Meier <rfmeier@gmail.com>
2019-06-17 15:43:38 -0500

    Added lib/widgets.php for widget callbacks.

---
+++ lib/widgets.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Widget callback functions.
+ *
+ * @package Genesis_Connect_WooCommerce
+ * @since 1.0.1
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+add_action( 'widgets_init', 'gencwooc_register_featured_products_widget' );
+/**
+ * Register Gencwooc_Featured_Products widget.
+ *
+ * Callback for WordPress 'widgets_init' action.
+ *
+ * @since 1.0.0
+ */
+function gencwooc_register_featured_products_widget() {
+
+	if ( class_exists( 'Gencwooc_Featured_Products' ) ) {
+		register_widget( 'Gencwooc_Featured_Products' );
+	}
+
+}
