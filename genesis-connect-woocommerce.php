0c4460906d8e9465ea47aa3da291e9e398137220
Ryan Meier <rfmeier@gmail.com>
2019-06-19 15:30:40 -0500

    Bumped WooCommerce tested-up-to-version to 3.6.4.

--- genesis-connect-woocommerce.php
+++ genesis-connect-woocommerce.php
@@ -11,7 +11,7 @@
  * License URI: http://www.opensource.org/licenses/gpl-license.php
  *
  * WC requires at least: 3.3.0
- * WC tested up to: 3.4
+ * WC tested up to: 3.6.4
  *
  * @package Genesis_Connect_WooCommerce
  *
